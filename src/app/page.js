9b718ecf7b249737cda744accf132fc3d2b18a2e
Joshua Comeau <joshwcomeau@gmail.com>
2023-05-01 11:29:31 -0400

    Opt into on-demand SSR with “cookies”

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import fs from 'fs';
 import path from 'path';
+import { cookies } from 'next/headers';
 
 const DATABASE_PATH = path.join(process.cwd(), 'src/database.json');
 
 import Counter from '../components/Counter';
 
 export default function Home() {
+  // HACK: Opt into “on-demand” SSR rendering
+  const cookieStore = cookies();
+
   const { hits } = JSON.parse(fs.readFileSync(DATABASE_PATH, 'utf8'));
 
   const newHits = hits + 1;
