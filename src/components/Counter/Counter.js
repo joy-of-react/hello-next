33be756465a57a654f268543692e5d4bccecae95
Joshua Comeau <joshwcomeau@gmail.com>
2023-05-01 11:19:52 -0400

    Hit counter example

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+
+function Counter() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <button onClick={() => setCount(count + 1)}>
+      Count: {count}
+    </button>
+  );
+}
+
+export default Counter;
