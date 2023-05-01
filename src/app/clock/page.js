c5380bf08f14068be1940ffdf1e145f25996707f
Joshua Comeau <joshwcomeau@gmail.com>
2023-05-01 17:07:23 -0400

    Minimal app

--- src/app/clock/page.js
+++
@@ -1,8 +0,0 @@
-// Always render dynamically, for accurate time
-export const dynamic = 'force-dynamic';
-
-export default function Clock() {
-  const renderAt = new Date().toISOString();
-
-  return <p>Page rendered at {renderAt}</p>;
-}
--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,7 @@
-import fs from 'fs';
-import path from 'path';
-import { cookies } from 'next/headers';
-
-const DATABASE_PATH = path.join(process.cwd(), 'src/database.json');
-
-import Counter from '../components/Counter';
-
 export default function Home() {
-  // HACK: Opt into “on-demand” SSR rendering
-  const cookieStore = cookies();
-
-  const { hits } = JSON.parse(fs.readFileSync(DATABASE_PATH, 'utf8'));
-
-  const newHits = hits + 1;
-
-  fs.writeFileSync(DATABASE_PATH, JSON.stringify({ hits: newHits }));
-
   return (
-    <html lang="en">
-      <body>
-        <main>
-          <h1>Hello Next!</h1>
-          <Counter />
-          <p>You are visitor number {hits}</p>
-        </main>
-      </body>
-    </html>
+    <main>
+      <h1>Hello Next!</h1>
+    </main>
   );
 }
