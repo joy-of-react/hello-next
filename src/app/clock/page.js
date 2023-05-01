63e802e9f3a98800e5fa05b82a1345d55c52322f
Joshua Comeau <joshwcomeau@gmail.com>
2023-05-01 14:03:07 -0400

    Fix it

--- src/app/clock/page.js
+++ src/app/clock/page.js
@@ -1,5 +1,5 @@
 // Always render dynamically, for accurate time
-export const dynamic = true;
+export const dynamic = 'force-dynamic';
 
 export default function Clock() {
   const renderAt = new Date().toISOString();
