c3aed7a5eac19d908d15710cc07535df987e0249
haalmarc <marcus.haaland@bekk.no>
2023-08-03 08:14:18 +0200

    footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react"
 
 function Home() {
   return (
     <main>
       <h1>Hello Next!</h1>
+      <footer>Her er en footer</footer>
     </main>
-  );
+  )
 }
 
-export default Home;
+export default Home
