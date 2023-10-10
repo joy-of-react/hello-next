d1d1fe5993a775f92314a64272db7b0d58ff498d
rviscomi <rviscomi@gmail.com>
2023-10-09 21:30:59 -0400

    footie

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,9 @@ function RootLayout({ children }) {
     <html lang="en">
       <body>
         {children}
-
+        <footer>
+          {new Date().toLocaleString()}
+        </footer>
       </body>
     </html>
   );
