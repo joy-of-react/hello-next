fc4eacd18c7dab032da2cf306452b3d5b601aecf
Kyle Bachand <kbachand@sbcglobal.net>
2023-10-24 13:14:44 -0500

    Added a simple timestamp to footer. Will be applied to all future pages as this was done in the layout file.

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,14 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+  const timestamp = new Date().toLocaleString();
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>
+          Page rendered on {timestamp}
+        </footer>
       </body>
     </html>
   );
