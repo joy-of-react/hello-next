025681c9d977bb31e5ca2b3832fd612f968637c1
Travis Amiel <travisamiel@users.noreply.github.com>
2024-02-01 15:22:16 -0500

    Update page.js with footer, for real

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,9 @@ function Home() {
       <h1>Hello Next!</h1>
       <p>I am learning so much thanks to this course</p>
     </main>
+    <footer>
+    Now there is a footer.
+    </footer>
   );
 }
 
