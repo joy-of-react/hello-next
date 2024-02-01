0283ccaa28855078fcdf0ba125e34b7d5bcd3e09
Travis Amiel <travisamiel@users.noreply.github.com>
2024-02-01 15:24:33 -0500

    Fix page

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 function Home() {
   return (
-    <main>
-      <h1>Hello Next!</h1>
-      <p>I am learning so much thanks to this course</p>
-    </main>
-    <footer>
-    Now there is a footer.
-    </footer>
+    <>
+      <main>
+        <h1>Hello Next!</h1>
+        <p>I am learning so much thanks to this course</p>
+      </main>
+      <footer>
+        Now there is a footer.
+      </footer>
+    </>
   );
 }
 
