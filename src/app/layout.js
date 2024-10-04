12d1219807290bf139423a3e78151f5f77b81a88
Antonio Contreras <dev@spiderwebrobot.com>
2024-10-04 07:39:14 -0700

    Adding footer to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
+import "./styles.css";
 
 function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>Copyright Josh Inc.</footer>
       </body>
     </html>
   );
