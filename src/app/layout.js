fb045819e2fa24edc869b599a3af2aa2e73c4555
kkauma <kylekauma@gmail.com>
2024-11-10 22:29:32 -0700

    add footer and time the page rendered

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
+import "./styles.css";
 
 function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>Page rendered on {new Date().toLocaleString()}</footer>
       </body>
     </html>
   );
